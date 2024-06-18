667f404597fea5c25d8bf493b37db958fc860f23
Nevaehel <sonja.hirvonen@gmail.com>
2024-06-18 22:12:46 +0300

    github pages deploy code

--- H/vite.config.js
+++ H/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ProjectH/', // This should match your repository name
 })
