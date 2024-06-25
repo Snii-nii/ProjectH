0a2fcfd383759ce5bcb6b1034f743510e92eace2
Nevaehel <sonja.hirvonen@gmail.com>
2024-06-25 16:25:37 +0300

    text

--- H/src/App.jsx
+++ H/src/App.jsx
@@ -1,14 +1,37 @@
 import './App.css'
 import "./assets/bluewood.jpg"
+
 function App() {
   
   return (
+    <>
     <div className="container">
       <div className="overlay">
         <h1>Häät</h1>
-        <p>Jotainjotain</p>
-      </div>
+          <p>Jotainjotain</p>
+
+        <h1>Vihkiminen ja juhlan kulku</h1>
+
+          <p>
+            Vihkitilaisuus alkaa Viikin kirkossa 3.8.2024, klo 11.             (Agronominkatu 5, 00790 Helsinki)
+            Vihkitilaisuuden jälkeen jatkamme juhlia Tammisalon VPK:n juhlatiloissa, n klo 12.00 (Tammisalontie 7, 00830 Helsinki) klo 21.30 saakka.
+
+            Ohjelma?
+          </p>
+
+        <h1>Liikkuminen & Parkkeeraus</h1>
+
+          <p>
+            Kirkon parkkipaikalta kadun varresta kirkon itäpäädystä löytyy 13 parkkipaikkaa, merkattu ”Kirkkovieraille”. Agronominkadun eteläpäässä, noin 200 metriä kirkolta, sijaitsee 30 neljän tunnin autopaikkaa. Myös Tilanhoitajankaaren varrella on yksittäisiä neljän tunnin autopaikkoja.
+
+            Matkaa juhlapaikalle on kirkolta noin 8 km. Siirtyminen juhlapaikalle tapahtuu omalla autolla, julkisilla, kimppakyydillä tai taksilla. Suositaan kimppakyytejä. Lisätietoa alla.
+
+            Juhlapaikalla VPK:lla paloautojen kulkureittejä ei saa tukkia, eikä palokunnan autopaikoille saa pysäköidä. Suosittelemme pysäköintiä Tammisalontielle, sekä tammisalon keskustasta löytyy muutama neljän tunnin autopaikka. Pyrimme tekemään opastekylttejä paikan päälle. Herttoniemen metroasemalla on myös ilmainen liityntäpysäköinti, josta on lyhyt bussimatka bussilla 83 juhlapaikalle.
+            Juhlapaikalle pääsee myös HSL busseilla 79 ja 83. Bussi 79 lähtee n. 200m päästä Latokartanonkaarelta ja vaihto 83:een Laivalahdenportilla tai Herttoniemen metroasemalla. Tammisalon VPK:n pysäkki on Leppisaarenkuja, josta matkaa jää 140 metriä. Huom! Tarvitset HSL-sovelluksen julkisilla liikkumiseen. Emme kuitenkaan pakota ketään turvautumaan julkiseen liikenteeseen, vaan ensisijaisesti toivomme saavamme vieraille tarpeeksi kimppakyytejä tai käyttävän taksipalveluita.
+          </p>
+        </div>
     </div>
+    </>
   )
 }
 
