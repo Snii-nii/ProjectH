5fa3d6f123db0eec8ac885d9cd5fa1ec1e56b417
Nevaehel <sonja.hirvonen@gmail.com>
2024-06-26 16:43:56 +0300

    adjusted css

--- H/src/App.jsx
+++ H/src/App.jsx
@@ -23,12 +23,24 @@ function App() {
 
           <p>
             Kirkon parkkipaikalta kadun varresta kirkon itäpäädystä löytyy 13 parkkipaikkaa, merkattu ”Kirkkovieraille”. Agronominkadun eteläpäässä, noin 200 metriä kirkolta, sijaitsee 30 neljän tunnin autopaikkaa. Myös Tilanhoitajankaaren varrella on yksittäisiä neljän tunnin autopaikkoja.
-
-            Matkaa juhlapaikalle on kirkolta noin 8 km. Siirtyminen juhlapaikalle tapahtuu omalla autolla, julkisilla, kimppakyydillä tai taksilla. Suositaan kimppakyytejä. Lisätietoa alla.
-
-            Juhlapaikalla VPK:lla paloautojen kulkureittejä ei saa tukkia, eikä palokunnan autopaikoille saa pysäköidä. Suosittelemme pysäköintiä Tammisalontielle, sekä tammisalon keskustasta löytyy muutama neljän tunnin autopaikka. Pyrimme tekemään opastekylttejä paikan päälle. Herttoniemen metroasemalla on myös ilmainen liityntäpysäköinti, josta on lyhyt bussimatka bussilla 83 juhlapaikalle.
-            Juhlapaikalle pääsee myös HSL busseilla 79 ja 83. Bussi 79 lähtee n. 200m päästä Latokartanonkaarelta ja vaihto 83:een Laivalahdenportilla tai Herttoniemen metroasemalla. Tammisalon VPK:n pysäkki on Leppisaarenkuja, josta matkaa jää 140 metriä. Huom! Tarvitset HSL-sovelluksen julkisilla liikkumiseen. Emme kuitenkaan pakota ketään turvautumaan julkiseen liikenteeseen, vaan ensisijaisesti toivomme saavamme vieraille tarpeeksi kimppakyytejä tai käyttävän taksipalveluita.
           </p>
+          <p>
+            Matkaa juhlapaikalle on kirkolta noin 8 km. Siirtyminen juhlapaikalle tapahtuu omalla autolla, julkisilla, kimppakyydillä tai taksilla. Suositaan kimppakyytejä. Lisätietoa alla.
+          </p>
+          <p>
+            Juhlapaikalla VPK:lla paloautojen kulkureittejä ei saa tukkia, eikä palokunnan autopaikoille saa pysäköidä. Suosittelemme pysäköintiä Tammisalontielle, sekä tammisalon keskustasta löytyy muutama neljän tunnin autopaikka. Pyrimme tekemään opastekylttejä paikan päälle. Herttoniemen metroasemalla on myös ilmainen liityntäpysäköinti, josta on lyhyt bussimatka bussilla 83 juhlapaikalle.
+          </p>
+          <p>
+            Juhlapaikalle pääsee myös HSL busseilla 79 ja 83. Bussi 79 lähtee n. 200m päästä Latokartanonkaarelta ja vaihto 83:een Laivalahdenportilla tai Herttoniemen metroasemalla. Tammisalon VPK:n pysäkki on Leppisaarenkuja, josta matkaa jää 140 metriä. Huom! Tarvitset HSL-
+            <a href="https://www.hsl.fi/liput-ja-hinnat/hsl-sovellus">sovelluksen </a>
+            julkisilla liikkumiseen. Emme kuitenkaan pakota ketään turvautumaan julkiseen liikenteeseen, vaan ensisijaisesti toivomme saavamme vieraille tarpeeksi kimppakyytejä tai käyttävän taksipalveluita.
+          </p>
+          <p><a href="https://www.taksihelsinki.fi/">Taxi-Helsinki</a> p. <span id="phone">&#48;&#49;&#48;&#48; &#48;&#55;&#48;&#48;</span></p>
+          <p><a href="https://www.kovanen.com/helsinkitaksi/"> Kovanen</a> p. <span id="phone">&#48;&#50;&#48;&#48; &#54;&#48;&#54;&#48;</span></p>
+          <p><a href="https://www.lahitaksi.fi/fi"> Lähitaksi</a> p. <span id="phone">&#48;&#49;&#48;&#48; &#55;&#51;&#48;&#48;</span></p>
+          <p><a href="https://meneva.fi/fi/palvelualueet/uusimaa/helsinki"> Menevä</a> p. <span id="phone">&#48;&#56;&#48;&#48; &#48;&#50;&#49;&#50;&#48;</span></p>
+
+
         </div>
     </div>
     </>
